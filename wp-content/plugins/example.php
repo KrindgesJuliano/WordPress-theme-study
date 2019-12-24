25d694365d2c68a71d51f68d94d3c11cdc111874
JulianoKrindges <juliano9krindges@hotmail.com>
2019-12-23 22:36:43 -0300

    initial activate of plugin

---
+++ wp-content/plugins/example.php
@@ -0,0 +1,33 @@
+<?php
+/* Plugin Name: Hooks Example */
+
+
+/* function ju_title($title){
+  return 'Hooked' . $title;
+}
+
+function ju_footer_shoutout(){
+  echo 'Hooked Example plugin was here  <br />';
+}
+
+
+function ju_footer_shoutout_v2(){
+  echo 'Hooked Example plugin was here. Version 2. <br />';
+}
+
+
+add_filter('the_title', 'ju_title');
+add_action('wp_footer', 'ju_footer_shoutout');
+add_action('wp_footer', 'ju_footer_shoutout_v2'); */
+
+function ju_footer(){
+  do_action('ju_custom_footer');
+}
+
+function ju_kill_wp(){
+  echo 'Hello';
+}
+
+
+add_action('wp_footer', 'ju_footer');
+add_action('ju_custom_footer', 'ju_kill_wp');
---
+++ wp-content/plugins/recipe/index.php
@@ -0,0 +1,25 @@
+<?php
+
+/* 
+Plugin Name:  Recipe
+Description: A Simple WordPress plugin that allows user to create recipes ans rate those recipes
+Version: 1.0
+Author: Udemy
+Author URI: https://udemy.com
+Text Domain: recipe
+*/
+
+if( !function_exists('add_action')){
+  echo "Hi there! i'm just a plugin, not much i can";
+  exit;
+}
+
+// Setup
+
+
+// Includes
+include('includes/activate.php');
+// Hooks
+register_activation_hook(__FILE__, 'r_activate_plugin');
+
+// Shortcodes
