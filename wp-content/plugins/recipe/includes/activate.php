53dd4e33730e29fe89251ac0079908f12b2127c8
JulianoKrindges <juliano9krindges@hotmail.com>
2019-12-29 23:55:54 -0300

    create template em table for rating

--- wp-content/plugins/recipe/includes/activate.php
+++ wp-content/plugins/recipe/includes/activate.php
@@ -4,4 +4,18 @@ function r_activate_plugin(){
   if(version_compare(get_bloginfo('version'), '5.0', '<')){
     wp_die(__("You must update WordPress to use this plugin.", 'recipe'));
   }
+
+  global $wpdb;
+  $createSQL      = "
+  CREATE TABLE `" . $wpdb->prefix ."recipe_ratings` (
+    `ID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+    `recipe_id` bigint(20) unsigned DEFAULT NULL,
+    `rating` float DEFAULT NULL,
+    `user_ip` varchar(50) DEFAULT NULL,
+    PRIMARY KEY (`ID`)
+  ) ENGINE=InnoDB " . $wpdb->get_charset_collate() . ";";
+
+
+  require( ABSPATH . "/wp-admin/includes/upgrade.php");
+  dbDelta($createSQL );
 }
