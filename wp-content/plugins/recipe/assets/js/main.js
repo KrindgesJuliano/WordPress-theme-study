ae61d6e4cafb2f9b348b346c8fbb8fd36642c0cf
JulianoKrindges <juliano9krindges@hotmail.com>
2020-01-05 17:10:36 -0300

    add rating to recipe with ajax

--- wp-content/plugins/recipe/assets/js/main.js
+++ wp-content/plugins/recipe/assets/js/main.js
@@ -1,15 +1,13 @@
-(function($){
-  $("#recipe_rating").bind( 'rateit', function(){
-    $(this).rateit('readonly', true);
+(function($) {
+  $("#recipe_rating").bind("rated", function() {
+    $(this).rateit("readonly", true);
 
-    var form      ={
-      action:         'r_rate_recipe',
-      rid:            $(this).data( 'rid'),
-      rating:         $(this).rateit( 'value')
+    var form = {
+      action: "r_rate_recipe",
+      rid: $(this).data("rid"),
+      rating: $(this).rateit("value")
     };
 
-    $.post( recipe_obj.ajax_url, form, function(data){
-      
-    })
+    $.post(recipe_obj.ajax_url, form, function(data) {});
   });
 })(jQuery);
